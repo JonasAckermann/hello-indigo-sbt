ae0f0aa207c8b2748f6ef62d2175a7334297ec87
JonasAckermann <jonas.p.ackermann@gmail.com>
2020-06-21 01:09:49 +0200

    score

--- src/main/scala/io/indigo/pluto/HelloIndigo.scala
+++ src/main/scala/io/indigo/pluto/HelloIndigo.scala
@@ -19,17 +19,65 @@ object HelloIndigo extends IndigoSandbox[Unit, Model] {
   val lineAssetName      = AssetName("centerStrip")
   val passengerAssetName = AssetName("passenger")
   val deliveryAssetName  = AssetName("delivery")
+  val fontAssetName = AssetName("font")
 
   val assets: Set[indigo.AssetType] =
     Set(
       AssetType.Image(AssetName("pluto"), AssetPath("assets/pluto.png")),
       AssetType.Image(AssetName("centerStrip"), AssetPath("assets/line.png")),
       AssetType.Image(AssetName("passenger"), AssetPath("assets/robert.png")),
-      AssetType.Image(AssetName("delivery"), AssetPath("assets/delivery.png"))
+      AssetType.Image(AssetName("delivery"), AssetPath("assets/delivery.png")),
+      AssetType.Image(AssetName("font"), AssetPath("assets/boxy_font.png"))
     )
 
+  val fontKey: FontKey = FontKey("font")
+
+  def fontInfo: FontInfo =
+    FontInfo(fontKey, Material.Textured(fontAssetName), 320, 230, FontChar("?", 93, 52, 23, 23))
+      .addChar(FontChar("A", 3, 78, 23, 23))
+      .addChar(FontChar("B", 26, 78, 23, 23))
+      .addChar(FontChar("C", 50, 78, 23, 23))
+      .addChar(FontChar("D", 73, 78, 23, 23))
+      .addChar(FontChar("E", 96, 78, 23, 23))
+      .addChar(FontChar("F", 119, 78, 23, 23))
+      .addChar(FontChar("G", 142, 78, 23, 23))
+      .addChar(FontChar("H", 165, 78, 23, 23))
+      .addChar(FontChar("I", 188, 78, 15, 23))
+      .addChar(FontChar("J", 202, 78, 23, 23))
+      .addChar(FontChar("K", 225, 78, 23, 23))
+      .addChar(FontChar("L", 248, 78, 23, 23))
+      .addChar(FontChar("M", 271, 78, 23, 23))
+      .addChar(FontChar("N", 3, 104, 23, 23))
+      .addChar(FontChar("O", 29, 104, 23, 23))
+      .addChar(FontChar("P", 54, 104, 23, 23))
+      .addChar(FontChar("Q", 75, 104, 23, 23))
+      .addChar(FontChar("R", 101, 104, 23, 23))
+      .addChar(FontChar("S", 124, 104, 23, 23))
+      .addChar(FontChar("T", 148, 104, 23, 23))
+      .addChar(FontChar("U", 173, 104, 23, 23))
+      .addChar(FontChar("V", 197, 104, 23, 23))
+      .addChar(FontChar("W", 220, 104, 23, 23))
+      .addChar(FontChar("X", 248, 104, 23, 23))
+      .addChar(FontChar("Y", 271, 104, 23, 23))
+      .addChar(FontChar("Z", 297, 104, 23, 23))
+      .addChar(FontChar("0", 3, 26, 23, 23))
+      .addChar(FontChar("1", 26, 26, 15, 23))
+      .addChar(FontChar("2", 41, 26, 23, 23))
+      .addChar(FontChar("3", 64, 26, 23, 23))
+      .addChar(FontChar("4", 87, 26, 23, 23))
+      .addChar(FontChar("5", 110, 26, 23, 23))
+      .addChar(FontChar("6", 133, 26, 23, 23))
+      .addChar(FontChar("7", 156, 26, 23, 23))
+      .addChar(FontChar("8", 179, 26, 23, 23))
+      .addChar(FontChar("9", 202, 26, 23, 23))
+      .addChar(FontChar("?", 93, 52, 23, 23))
+      .addChar(FontChar("!", 3, 0, 15, 23))
+      .addChar(FontChar(".", 286, 0, 15, 23))
+      .addChar(FontChar(",", 248, 0, 15, 23))
+      .addChar(FontChar(" ", 145, 52, 23, 23))
+
   val fonts: Set[indigo.FontInfo] =
-    Set()
+    Set(fontInfo)
 
   def setup(
       assetCollection: indigo.AssetCollection,
@@ -87,10 +135,11 @@ object HelloIndigo extends IndigoSandbox[Unit, Model] {
       context: indigo.FrameContext,
       model: Model
   ): indigo.SceneUpdateFragment =
-    SceneUpdateFragment(
-    ).addGameLayerNodes(
+    SceneUpdateFragment.empty
+    .addGameLayerNodes(
       drawScene(model.pluto, model.lines, model.passenger, model.deliveryStop)
     )
+    .addGameLayerNodes(drawText(model.passenger.score))
 
   def drawScene(
       pluto: Pluto,
@@ -119,6 +168,8 @@ object HelloIndigo extends IndigoSandbox[Unit, Model] {
           .moveTo(pluto.location)
       )
 
+  def drawText(score: Int) = Text(s"Score $score", 10, 20, 1, fontKey).alignLeft
+
 }
 
 case class Model(pluto: Pluto, lines: List[CenterStrip], passenger: Passenger, deliveryStop: DeliveryStop, ftct: Int) {
@@ -133,7 +184,7 @@ case class Model(pluto: Pluto, lines: List[CenterStrip], passenger: Passenger, d
 object Model {
   def initial(center: Point): Model =
     Model(
-      Pluto(center, 0),
+      Pluto(Point(center.x, center.y - 300), 0),
       (0 to 970 by 88).map(y => CenterStrip(Point(center.x, y), -gamespeed)).toList,
       Passenger.initial(center.x - 100),
       DeliveryStop(Point(center.x, center.y)),
@@ -155,9 +206,9 @@ case class CenterStrip(location: Point, velocity: Double) {
   }
 }
 
-case class Passenger(state: Pickup, location: Point, drawLocation: Point) {
+case class Passenger(state: Pickup, location: Point, drawLocation: Point, score: Int) {
   def update(plutoPos: Point, deliveryPos: Point, input: Command): Passenger = {
-    val (newState, newLocation, newDrawLocation) = plutoPos match {
+    val (newState, newLocation, newDrawLocation, newScore): Tuple4[Pickup, Point, Point, Int] = plutoPos match {
       // don't override success state, check if delivery
       case _ if state == Pickup.Success =>
         input match {
@@ -165,38 +216,37 @@ case class Passenger(state: Pickup, location: Point, drawLocation: Point) {
             (
               Pickup.Delivered,
               Point(deliveryPos.x + 40, deliveryPos.y + 25),
-              Point(deliveryPos.x + 40, deliveryPos.y + 25)
+              Point(deliveryPos.x + 40, deliveryPos.y + 25),
+            score + 10
             )
-          case _ =>
-            println(s"In check, but condition false for pluto $plutoPos, del $deliveryPos")
-            (Pickup.Success, Point(location.x, location.y - gamespeed), Point(-100, -100))
+          case _ => (Pickup.Success, Point(location.x, location.y - gamespeed), Point(-100, -100), score)
         }
       // higher than view, reset
-      case _ if location.y < -25 && state != Pickup.Success => Passenger.placeRandom
+      case _ if location.y < -25 && state != Pickup.Success => Passenger.placeRandom(score)
       // higher than pluto, maybe missed
       case a if a.y > (location.y + 100) =>
         state match {
-          case Pickup.Success => (Pickup.Success, Point(location.x, location.y - gamespeed), Point(-100, -100))
+          case Pickup.Success => (Pickup.Success, Point(location.x, location.y - gamespeed), Point(-100, -100), score)
           case _ =>
-            (Pickup.Missed, Point(location.x, location.y - gamespeed), Point(location.x, location.y - gamespeed))
+            (Pickup.Missed, Point(location.x, location.y - gamespeed), Point(location.x, location.y - gamespeed), score)
         }
       // out of reach, still possible
       case a if a.y < (location.y - 20) =>
-        (Pickup.NotYet, Point(location.x, location.y - gamespeed), Point(location.x, location.y - gamespeed))
+        (Pickup.NotYet, Point(location.x, location.y - gamespeed), Point(location.x, location.y - gamespeed), score)
       // possible pickup, depending on position and button
       case l if l.x >= location.x - 20 && l.x <= location.x + 95 && state != Pickup.Success =>
         input match {
           // picked up, hide
-          case Command.Pickup => (Pickup.Success, Point(location.x, location.y - gamespeed), Point(-100, -100))
+          case Command.Pickup => (Pickup.Success, Point(location.x, location.y - gamespeed), Point(-100, -100), score)
           // not picked up, try again
           case _ =>
-            (Pickup.NotYet, Point(location.x, location.y - gamespeed), Point(location.x, location.y - gamespeed))
+            (Pickup.NotYet, Point(location.x, location.y - gamespeed), Point(location.x, location.y - gamespeed), score)
         }
-      case _ => (Pickup.NotYet, Point(location.x, location.y - gamespeed), Point(location.x, location.y - gamespeed))
+      case _ => (Pickup.NotYet, Point(location.x, location.y - gamespeed), Point(location.x, location.y - gamespeed), score)
     }
 
     //println(s"State: $state, plutoPos: $plutoPos, deliveryPos: $deliveryPos, passenger: $location, input: $input")
-    this.copy(newState, newLocation, newDrawLocation)
+    this.copy(newState, newLocation, newDrawLocation, newScore)
 
   }
 
@@ -216,12 +266,12 @@ case class Passenger(state: Pickup, location: Point, drawLocation: Point) {
 object Passenger {
   def initial(xLoc: Int) = {
     val (state, loc, dloc) = reset(xLoc)
-    Passenger(state, loc, dloc)
+    Passenger(state, loc, dloc, 0)
   }
   def reset(xLoc: Int) = (Pickup.NotYet, Point(xLoc, height), Point(xLoc, height))
-  def placeRandom = {
+  def placeRandom(score: Int) = {
     val xLoc = scala.util.Random.between(10, width - 10)
-    (Pickup.NotYet, Point(xLoc, height), Point(xLoc, height))
+    (Pickup.NotYet, Point(xLoc, height), Point(xLoc, height), score)
   }
 }
 
